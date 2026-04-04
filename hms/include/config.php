6e6607d968b6a9152ff2efd5ef5dfb44abd4ab79
*Roshan <*roshanchavan810@gmail.com*>
2026-04-04 10:42:50 +0530

    enable secure SSL connection

--- hms/include/config.php
+++ hms/include/config.php
@@ -1,9 +1,19 @@
 <?php
 
-$con = mysqli_connect(getenv("DB_HOST"),getenv("DB_USER"),getenv("DB_PASSWORD"),getenv("DB_NAME"),getenv("DB_PORT"));
-// Check connection
-if (mysqli_connect_errno())
-{
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASSWORD");
+$db   = getenv("DB_NAME");
+$port = (int)getenv("DB_PORT");
+
+$con = mysqli_init();
+
+mysqli_ssl_set($con, NULL, NULL, NULL, NULL, NULL);
+
+mysqli_real_connect($con, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
+
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
 ?>
